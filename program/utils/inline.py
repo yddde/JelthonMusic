fa87eb56f5a1466d3145618f9825aa2e80c326b0
yddde <93260463+yddde@users.noreply.github.com>
2022-07-03 01:27:44 +0200

    Add files via upload

---
+++ program/utils/inline.py
@@ -0,0 +1,63 @@
+""" inline section button """
+
+from pyrogram.types import (
+  CallbackQuery,
+  InlineKeyboardButton,
+  InlineKeyboardMarkup,
+  Message,
+)
+
+
+def stream_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="✶ التحكم", callback_data=f'cbmenu | {user_id}'),
+      InlineKeyboardButton(text="✶ القناة", url=f'https://t.me/JelthonMusic'),
+    ],
+    [
+    InlineKeyboardButton(
+                        "✶ اضف البوت الى مجموعتك",
+                        url=f'https://t.me/MusicInCaIIBot?startgroup=true'),
+    ],
+  ]
+  return buttons
+
+
+def menu_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="⏹", callback_data=f'cbstop | {user_id}'),
+      InlineKeyboardButton(text="⏸", callback_data=f'cbpause | {user_id}'),
+      InlineKeyboardButton(text="▶️", callback_data=f'cbresume | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🔇", callback_data=f'cbmute | {user_id}'),
+      InlineKeyboardButton(text="🔊", callback_data=f'cbunmute | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="✶ اغلاق", callback_data='cls'),
+    ]
+  ]
+  return buttons
+
+
+close_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "✶ رجوع", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
+
+
+back_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "✶ رجوع", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
